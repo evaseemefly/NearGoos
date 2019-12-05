20580ec437caf827c8da67d4a94d2a64cc1e03a1
QuYuan891211 <quyuan891211@163.com>
2019-12-05 18:41:08 +0800

    Data页面---->center-header部分---->数据概述文字内容完成

--- webclient/src/views/content/Data.vue
+++ webclient/src/views/content/Data.vue
@@ -1,5 +1,26 @@
 <template>
-  <div></div>
+  <div class="center">
+    <div class="center-header">
+      <div class="buoy-overview">
+        <h3>Buoy</h3>
+          <div class="content">
+          <p>North Sea Buoy hourly data including elements: buoy id with longitude and latitude; year, month, date, hour (UTC); sea level air pressure, sea level air temperature; significant wave height, maximum wave height; sea surface temperature; data file format .csv.</p>
+          </div>
+      </div>
+      <div class="station-overview">
+        <h3>Station</h3>
+          <div class="content">
+          <p>14 stations hourly data including elements: station name with longitude and latitude; year, month, date, hour (UTC); wind direction, wind speed; sea level air pressure, sea level air temperature; wave period, significant wave height, maximum wave height; swell direction, swell period, swell height; sea surface temperature; data file format .csv.</p>
+          </div>
+      </div>
+      <div class="Ship-overview">
+        <h3>Chinese volunteer ships</h3>
+          <div class="content">
+          <p>Hourly data including elements: ship name with longitude and latitude; year, month, date, hour (UTC); wind direction, wind speed; sea level air pressure, sea level air temperature; data file format .csv.</p>
+          </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue, Watch } from "vue-property-decorator";
